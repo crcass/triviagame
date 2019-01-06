24b080ddedc6f1f247b97f54753b08d9ac56bab7
crcass <ccass@live.com>
2019-01-05 20:15:17 -0600

    core game logic complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
 			$('#c3').text(this.choices[2]);
 			$('#c4').text(this.choices[3]);
 			answer = this.choices[1];
-
 		}
 	}
 
@@ -27,26 +26,120 @@ $(document).ready(function() {
 			$('#c3').text(this.choices[2]);
 			$('#c4').text(this.choices[3]);
 			answer = this.choices[0];
-
 		}
 	}
 
-	// answers
-	// const answerKey = {
-	// 	one: questionOne.choices[1]
-	// }
+	const questionThree = {
+		clue: 'This is question 3',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
 
-	// question array
-	let questions = [questionOne, questionTwo];
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
 
-	// global variables
-	let j = 0;
-	let playerChoice;
-	let answer;
-	let correct;
-	let wrong;
-	let wins = 0;
-	let losses = 0;
+	const questionFour = {
+		clue: 'This is question 4',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
+
+	const questionFive = {
+		clue: 'This is question 5',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
+
+	const questionSix = {
+		clue: 'This is question 6',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
+
+	const questionSeven = {
+		clue: 'This is question 7',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
+
+	const questionEight = {
+		clue: 'This is question 8',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
+
+	const questionNine = {
+		clue: 'This is question 9',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
+
+	const questionTen = {
+		clue: 'This is question 10',
+		choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4'],
+
+		setup() {
+			$('#clue').text(this.clue);
+			$('#c1').text(this.choices[0]);
+			$('#c2').text(this.choices[1]);
+			$('#c3').text(this.choices[2]);
+			$('#c4').text(this.choices[3]);
+			answer = this.choices[1];
+		}
+	}
 
 	const game = {
 		setup() {
@@ -56,42 +149,86 @@ $(document).ready(function() {
 		},
 
 		start() {
+			questions[j].setup();
 			$('.status').css('display', 'none');
 			$('#start').css('display', 'none');
 			$('#clue').css('display', 'block');
 			$('.timer').css('display', 'inline-block');
 			$('.choice').css('display', 'block');
+			$('#timer-text').text('Time Remaining:');
+			gameTimer = setTimeout(game.timeUp, 10000);
 		},
 
-		// FINISH TIMER AFTER QUESTIONS & GAME LOOP ARE WORKING
-		// qTimer() {
-		// 	setTimeout()
-		// }
+		post() {
+			j++;
+			console.log(j);
+			$('.status').css('display', 'block');
+			$('#clue').css('display', 'none');
+			$('.choice').css('display', 'none');
+			$('#timer-text').text('Next Round Starts In:');
+			setTimeout(game.start, 5000);
+		},
+
+		timeUp() {
+			j++;
+			console.log(j);
+			$('.status').css('display', 'block');
+			$('#clue').css('display', 'none');
+			$('.choice').css('display', 'none');
+			$('#stat-head').text('Time\'s Up!');
+			$('#stat-p').text(`The correct answer is ${answer}`);
+			$('#timer-text').text('Next Round Starts In:');
+			setTimeout(game.start, 5000);
+		}
+
 	}
+
+	// question array
+	let questions = [
+			questionOne,
+			questionTwo,
+			questionThree,
+			questionFour,
+			questionFive,
+			questionSix,
+			questionSeven,
+			questionEight,
+			questionNine,
+			questionTen
+	];
+
+	// global variables
+	let j = 0;
+	let playerChoice;
+	let answer;
+	let wins = 0;
+	let losses = 0;
+	let gameTimer;
 
 	// setup game board
 	game.setup();
 
-
 	$('#start').on('click', function() {
+		console.log(j);
 		game.start();
-		questions[j].setup();
 	})
-
 
 	$('.choice').on('click', function() {
 		playerChoice = $(this).text();
-		// console.log(playerChoice);
+		clearTimeout(gameTimer);
 		if (playerChoice === answer) {
-			console.log('SUCCESS');
-			j++;
 			wins++;
-			questions[j].setup();
-		} else {
-			console.log('FAILURE');
-			j++;
+			console.log('correct');
+			game.post();
+			$('#stat-head').text('Good Job!');
+			$('#stat-p').text(`The correct answer is ${answer}`);
+		} 
+		else {
 			losses++;
-			questions[j].setup();
+			console.log('wrong');
+			game.post();
+			$('#stat-head').text('Too Bad!');
+			$('#stat-p').text(`The correct answer is ${answer}`);
 		}
 	})
 
